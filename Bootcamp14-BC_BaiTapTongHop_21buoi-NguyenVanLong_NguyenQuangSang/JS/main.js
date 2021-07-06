195f5924657b679b1edf6fc916e0d741ff40c8a7
Nguyen Quang Sang <nguyenquangsang1991@gmail.com>
2021-07-06 23:44:42 +0700

    UP File Bai Tap

---
+++ Bootcamp14-BC_BaiTapTongHop_21buoi-NguyenVanLong_NguyenQuangSang/JS/main.js
@@ -0,0 +1,8 @@
+// switch theme
+
+document.getElementById("checkbox").onclick = function(){
+    document.getElementById("body").classList.toggle("theme-dark")
+    console.log("ádasdas");
+    
+}
+
